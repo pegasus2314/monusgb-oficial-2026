2af8a6055601cf348eaae3a247c5f32baae5e1c3
pegasus2314 <itsalberthjesus@gmail.com>
2026-04-04 23:34:12 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next'
 import { Anton, Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
-import { AuthProvider } from '@/lib/auth.tsx'
+import { AuthProvider } from '@/lib/auth'
 const inter = Inter({
   subsets: ['latin'],
   variable: '--font-inter',
